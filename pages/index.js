9e5a77d1fbec5bbae1596460500d69927bb0f1ee
saharmybae <alcatrazblaz@gmail.com>
2025-05-16 04:57:15 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 export default function Home() {
   return (
     <main style={{
-      display:'flex',justifyContent:'center',
-      alignItems:'center',height:'100vh',
-      color:'#fff',background:'#000'
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+      height:'100vh',
+      background:'#000',
+      color:'#fff',
+      fontFamily:'Playfair Display, serif'
     }}>
       <h1>Sahâr Beauty Lounge</h1>
     </main>
